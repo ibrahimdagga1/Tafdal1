0ee2bd28de980f8f0ea33254fb0567b45d31b707
ibrahimdagga <ibrahimbagga09@gmail.com>
2021-04-01 11:27:16 +0300

    edit background button

--- app/src/main/java/com/example/tafdal/Register.java
+++ app/src/main/java/com/example/tafdal/Register.java
@@ -1,24 +1,37 @@
 package com.example.tafdal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.tafdal.databinding.ActivityLoginBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
 
     EditText et_name , et_mobile ,et_phone,et_title,et_email,et_password;
     Button btn_reg ;
+    private ProgressBar progressBar;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
 
         et_name = findViewById(R.id.register_et_name);
         et_email = findViewById(R.id.register_et_email);
@@ -27,21 +40,57 @@ public class Register extends AppCompatActivity {
         et_title = findViewById(R.id.register_et_title);
         et_password = findViewById(R.id.register_et_password);
         btn_reg = findViewById(R.id.register_btn_login);
+        progressBar =findViewById(R.id.progressBar);
 
         btn_reg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent mainIntent = new Intent(Register.this,Login.class);
-                Register.this.startActivity(mainIntent);
-                Register.this.finish();
+                String email = et_email.getText().toString().trim();
+                String password = et_password.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+
+                //create user
+                auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(Register.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                                progressBar.setVisibility(View.GONE);
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(Register.this, "Authentication failed." + task.getException(),
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    startActivity(new Intent(Register.this, MainActivity.class));
+                                    finish();
+                                }
+                            }
+                        });
             }
         });
 
+    }
 
-
-
-
-
-
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(View.GONE);
     }
 }
